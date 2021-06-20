e98a5185468caaecec384fb843bb00d11ea46b3f
mansouralbatran <mansouralbatran@gmail.com>
2021-06-20 18:23:07 +0300

    lab01

---
+++ lab01.js
@@ -0,0 +1,9 @@
+let yourName = prompt("what is your name")
+ console.log(typeof yourName);
+let fM = prompt("what is your faverotmeal")
+console.log(typeof fM);
+let fD = prompt("what is your favertdrink")
+console.log(typeof fD);
+let rest = prompt("which kind of restorant do you prefer")
+console.log(typeof rest);
+alert("welome" + yourName + "ohh relly and i like " + fM + "very much but i dont like " + fD + "but also iagree whith you about " + rest)
